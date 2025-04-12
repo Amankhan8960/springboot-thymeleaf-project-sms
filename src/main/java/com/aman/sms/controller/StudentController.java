baf16cdbc216bca979d92cd5d4ffb5ef2268e15d
Amankhan8960 <aman531305@gmail.com>
2025-04-13 05:26:12 +0530

    Removing some unused classes and adding Service implementation and thymeleaf html

--- src/main/java/com/aman/sms/controller/StudentController.java
+++ src/main/java/com/aman/sms/controller/StudentController.java
@@ -1,7 +1,26 @@
 package com.aman.sms.controller;
 
+import com.aman.sms.services.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        super();
+        this.studentService = studentService;
+    }
+
+
+    // handler method to handle list students and return mode and view
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+        model.addAttribute("students", studentService.getAllStudents());
+        System.out.println("Returning view: students");
+        return "students";
+    }
 }
--- src/main/java/com/aman/sms/services/impl/StudentServiceImpl.java
+++ src/main/java/com/aman/sms/services/impl/StudentServiceImpl.java
@@ -1,6 +1,25 @@
 package com.aman.sms.services.impl;
 
+import com.aman.sms.entity.Student;
+import com.aman.sms.repository.StudentRepository;
 import com.aman.sms.services.StudentService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class StudentServiceImpl implements StudentService {
+
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        super();
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
